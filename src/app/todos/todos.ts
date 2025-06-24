516b51e17ff133b3cac909e01e30b376c7e40c25
Vogias11 <avogiajis@gmail.com>
2025-06-24 14:26:46 +0300

    24-06-25

--- src/app/todos/todos.ts
+++ src/app/todos/todos.ts
@@ -2,10 +2,12 @@ import { Component,inject,signal,OnInit } from '@angular/core';
 import { TodosServices } from '../services/todos';
 import { Todo } from '../components/model/todo.type';
 import { catchError } from 'rxjs';
+import { TodoItem } from '../components/todo-item/todo-item';
+
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [TodoItem],
   templateUrl: './todos.html',
   styleUrl: './todos.scss'
 })
@@ -24,5 +26,18 @@ export class Todos implements OnInit {
     .subscribe((todos) => {
       this.todoItems.set(todos);
     });
+  }
+  updateTodoItem(todoItem: Todo) {
+  this.todoItems.update((todos) => {
+  return todos.map(todo => {
+    if (todo.id === todoItem.id) {
+      return {
+        ...todo,
+        completed: !todo.completed,
+      };
+    }
+    return todo; // Always return something
+  });
+});
   }
 }
